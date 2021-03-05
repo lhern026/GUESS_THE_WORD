137abc377e70988d9f1434c7b8b3e0163dda64a4
luis hernandez <luishernandez@luiss-MacBook-Air.local>
2021-03-05 14:37:31 -0800

    testing

--- js/main.js
+++ js/main.js
@@ -17,3 +17,13 @@
 
 
 /*----- functions -----*/
+1.callback functions
+2.yes
+3.yes
+4.to create objects
+5.to initalize data properties
+6.static methods can be called by instances
+7.assigns toliver to the class Dog
+8.No
+9.it will filter out 
+10.51
