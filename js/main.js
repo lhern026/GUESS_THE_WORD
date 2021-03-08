abde802c969b94f6b1657ebf9925fabb066b1686
luis hernandez <luishernandez@luiss-MacBook-Air.local>
2021-03-08 08:35:57 -0800

    Board is ready to render

--- js/main.js
+++ js/main.js
@@ -1,23 +1,55 @@
 /*----- constants -----*/
-let words= ['donkey', 'monkey', 'picnic', 'hungry', 'covers', 'friend']
-let pictures= []
+const words = ['donkey', 'monkey', 'picnic', 'hungry', 'covers', 'friend'];
+
 
 
 /*----- app's state (variables) -----*/
-let winner, grid, word;
+let winner, keyboard, movesLeft ;
 
 
 
 /*----- cached element references -----*/
 
-document.querySelector(['#replay']);
-    addEventListener("click",  )
+const msgEl = document.getElementById('msg');
+const replayBtn = document.querySelector('button');
+const usedLetters = [...document.querySelectorAll('#keyboard > div')];
+const guessDisplay = document.querySelector('.display');
+const turnsLeft = document.querySelector('h3');
+
 
 
 /*----- event listeners -----*/
-
+document.querySelector('.keyboard')
+    .addEventListener('click', handle);
+    
+    
+replayBtn.addEventListener('click', init);
 
 
 
 /*----- functions -----*/
+init();
+function handle(evt){
+    
+}
+function init(){
+    winner = null;
+    movesLeft = 10;
+    keyboard = [
+        ['a', 'b', 'c', 'd', 'e', 'f', 'g' ,
+        'h', 'i', 'j', 'k', 'l', 'm', 'n' ,
+        'o', 'p', 'q', 'r', 's', 't', 'u' ,
+        'v', 'w', 'x', 'y', 'z']
+    ];
+    render();
+
+}
+function render(){
+    //rendering the board
+    
+    
+
+    })
+
+    }
 
